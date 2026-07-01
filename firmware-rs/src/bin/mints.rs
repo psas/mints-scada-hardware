3e988df69655dbbcd580d5bac934d4b1613a40cf
William Jeffreys <wjeffreys96@gmail.com>
2026-07-01 14:36:45 -0700

    Use g4 CAN example and adapt for our g0
    I grabbed the CAN example for the g4 from embassy's repo and adapted it
    to work for our g0. I'm almost positive the RCC config is not the best,
    so we need to look into how that works.

--- firmware-rs/src/bin/mints.rs
+++ firmware-rs/src/bin/mints.rs
@@ -3,63 +3,85 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-// use embassy_stm32::peripherals::CAN1;
-// use embassy_stm32::peripherals::pb8;
-use embassy_stm32::{
-    bind_interrupts,
-    can::{
-        Can, Fifo, Frame, Rx0InterruptHandler, Rx1InterruptHandler, SceInterruptHandler,
-        StandardId, TxInterruptHandler, filter::Mask32,
-    },
-    peripherals::CAN,
-};
-use embassy_time::Instant;
+use embassy_stm32::{bind_interrupts, can, peripherals::FDCAN1, time::Hertz};
+use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
-bind_interrupts!(struct CanIrqs {
-    CEC_CAN =>
-    Rx0InterruptHandler<CAN>,
-    Rx1InterruptHandler<CAN>,
-    SceInterruptHandler<CAN>,
-    TxInterruptHandler<CAN>;
+bind_interrupts!(struct Irqs {
+    TIM16_FDCAN_IT0 => can::IT0InterruptHandler<FDCAN1>;
+    TIM17_FDCAN_IT1 => can::IT1InterruptHandler<FDCAN1>;
 });
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    info!("Hello World!");
+    let mut config = embassy_stm32::Config::default();
+    info!("creating peripherals config");
+    {
+        // TODO: Check these values. They work but are likely not ideal
+        use embassy_stm32::rcc::*;
+        info!("hsi");
+        config.rcc.hsi48 = Some(Hsi48Config::default());
+        info!("pll");
+        config.rcc.pll = Some(Pll {
+            source: PllSource::HSI,
+            prediv: PllPreDiv::DIV1,
+            mul: PllMul::MUL8, // Mult factor for VCO
+            divp: None, // ADC
+            divq: Some(PllQDiv::DIV2), // USB, I2S23, SAI1, FDCAN, QSPI
+            divr: Some(PllRDiv::DIV2), // SYSCLK
+        });
+        info!("fdcansel");
+        config.rcc.mux.fdcansel = mux::Fdcansel::PLL1_Q;
+        info!("sys");
+        config.rcc.sys = Sysclk::PLL1_R;
+    }
 
-    let mut p = embassy_stm32::init(Default::default());
+    info!("configuring peripherals");
+    let peripherals = embassy_stm32::init(config);
 
-    info!("initializing CAN driver");
-    let mut can = Can::new(p.CAN, p.PA11, p.PA12, CanIrqs);
-    can.modify_filters()
-        .enable_bank(0, Fifo::Fifo0, Mask32::accept_all());
+    info!("Configuratoring CAN");
+    let mut can =
+        can::CanConfigurator::new(peripherals.FDCAN1, peripherals.PA11, peripherals.PA12, Irqs);
 
-    info!("configuring CAN");
-    can.modify_config()
-        .set_loopback(false) // Receive own frames
-        .set_silent(false)
-        .set_bitrate(1_000_000);
+    info!("setting filters");
+    can.properties().set_extended_filter(
+        can::filter::ExtendedFilterSlot::_0,
+        can::filter::ExtendedFilter::accept_all_into_fifo1(),
+    );
 
-    info!("enabling CAN");
-    can.enable().await;
+    can.set_bitrate(1_000_000);
+    can.set_fd_data_bitrate(1_000_000, false);
+    info!("Configured CAN");
 
-    let mut i: u8 = 0;
+    // TODO: Test normal operating mode, not just loopback
+    let mut can = can.start(can::OperatingMode::InternalLoopbackMode);
+
+    let mut i = 0;
+    let mut last_read_ts = embassy_time::Instant::now();
+
     loop {
-        let tx_frame = Frame::new_data(unwrap!(StandardId::new(i as _)), &[i]).unwrap();
-        can.write(&tx_frame).await;
-        info!("Waiting....");
+        let frame = can::frame::FdFrame::new_extended(0x123456F, &[i; 16]).unwrap();
+        info!("Writing frame");
 
-        match can.read().await {
+        _ = can.write_fd(&frame).await;
+
+        match can.read_fd().await {
             Ok(envelope) => {
-                info!("envelope is defined");
-                info!("loopback frame {}", envelope.frame.data());
+                let (ts, rx_frame) = (envelope.ts, envelope.frame);
+                let delta = (ts - last_read_ts).as_millis();
+                last_read_ts = ts;
+                info!(
+                    "Rx: {} {:02x} --- using FD API {}ms",
+                    rx_frame.header().len(),
+                    rx_frame.data()[0..rx_frame.header().len() as usize],
+                    delta,
+                )
             }
-            Err(e) => {
-                info!("{}", e)
-            }
+            Err(_err) => error!("Error in frame"),
         }
 
-        i = i.wrapping_add(1);
+        Timer::after_millis(250).await;
+
+        i += 1;
     }
 }
